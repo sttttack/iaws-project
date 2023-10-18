a810f153558808da7d3198f1d30efbb56dc31b52
Papuna Maglaperidze <papunamaglaperidze@Papunas-MacBook-Pro.local>
2023-10-18 16:46:13 -0700

    replaced png with svg for card icons

--- src/UI/Card/ValueCard.jsx
+++ src/UI/Card/ValueCard.jsx
@@ -1,12 +1,12 @@
-const ValueCard = ({ image, title, description, altTitle }) => {
+const ValueCard = ({ image, title, description }) => {
   return (
     <div className="flex justify-center items-center flex-col rounded-[60px] bg-white w-[344px] h-[394px]">
-      <img src={image} alt={altTitle} />
-      <div className="p-6">
+      <div>{image}</div>
+      <div className="pt-10">
         <h5 className="mb-2 text-xl font-semibold leading-tight text-neutral-800 text-center">
           {title}
         </h5>
-        <p className="mb-4 text-base font-light text-neutral-600 text-center">
+        <p className="mb-4 text-base font-light text-neutral-600 text-center w-[270px]">
           {description}
         </p>
       </div>

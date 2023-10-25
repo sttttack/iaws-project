dd44d410dd8069f34dbd508fe0b783ad092b635a
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-10-25 01:47:15 -0700

    created registation form

--- src/UI/Registation/Registation.jsx
+++ src/UI/Registation/Registation.jsx
@@ -1,5 +1,84 @@
-const Registation = () => {
-  return <>aa</>;
-};
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export default Registation;
+function Registration() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+    console.log(formData);
+  };
+
+  return (
+    <div className="flex flex-col justify-center container items-center mx-auto mt-8">
+      <NavLink to="/">Back to main page</NavLink>
+      <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+        <h1 className="text-3xl font-semibold mb-4">Sign Up</h1>
+        <div className="mb-4">
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            placeholder="First Name"
+            className="w-full p-2 rounded border"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            placeholder="Last Name"
+            className="w-full p-2 rounded border"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Email"
+            className="w-full p-2 rounded border"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Password"
+            className="w-full p-2 rounded border"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
